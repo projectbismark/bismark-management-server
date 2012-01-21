398b9e1c168611b7f70b2b0531d97e6c68fd1094
Stephen Woodrow <srwoodrow@gmail.com>
2012-01-21 02:10:02 -0500

    Make check for virtualenv simpler.

--- bdmd/mkvirtualenv.sh
+++ bdmd/mkvirtualenv.sh
@@ -2,13 +2,10 @@
 
 VENV_DIR=virt-python
 PIP_CMD="pip" # $(which pip)
-VENV_CMD=$(which virtualenv)
+VENV_CMD=$(which virtualenv2 || which virtualenv)
 if [ $? -ne 0 ]; then
-    VENV_CMD=$(which virtualenv2)
-    if [ $? -ne 0 ]; then
-        echo "Can't find virtualenv. You probably need to install it."
-        exit 1
-    fi
+    echo "Can't find virtualenv. You probably need to install it."
+    exit 1
 fi
 
 $VENV_CMD --no-site-packages --distribute $VENV_DIR
