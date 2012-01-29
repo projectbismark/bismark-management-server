215f0adf3d05733deb3cf4f8c6ec4ef2f6ebb83e
Stephen Woodrow <srwoodrow@gmail.com>
2012-01-29 13:52:52 -0500

    Move target ip address into separate table.
    - New table target_ips allows target ip addresses to change independently of
      the targets table.
    - Also remove zone and cat columns from targets table. This would be better
      handled by a general notion of grouping, and only if we need it later.

--- db/bismark_mgmt_tables.sql
+++ db/bismark_mgmt_tables.sql
@@ -16,59 +16,62 @@ CREATE DOMAIN mtype_t AS varchar(50);
 CREATE DOMAIN fqdn_t AS varchar(255);
 
 CREATE TABLE devices (
-    id          id_t        PRIMARY KEY,
-    bversion    version_t   NOT NULL,
-    duser       user_t      NOT NULL,
-    ip          ip_t        NOT NULL,
-    ts          ts_t        NOT NULL
+    id              id_t            PRIMARY KEY,
+    bversion        version_t       NOT NULL,
+    duser           user_t          NOT NULL,
+    ip              ip_t            NOT NULL,
+    ts              ts_t            NOT NULL
 );
 
 CREATE TABLE tunnels (
-    device_id   id_t        NOT NULL REFERENCES devices (id),
-    port        integer     NOT NULL,
-    ts          ts_t        NOT NULL,
+    device_id       id_t            NOT NULL REFERENCES devices (id),
+    port            integer         NOT NULL,
+    ts              ts_t            NOT NULL,
     PRIMARY KEY (device_id, port)
 );
 
 CREATE TABLE messages (
-    rowid       serial      PRIMARY KEY,
-    msgfrom     msg_addr_t  NOT NULL,
-    msgto       msg_addr_t  NOT NULL,
-    msg         msg_t       NOT NULL
+    rowid           serial          PRIMARY KEY,
+    msgfrom         msg_addr_t      NOT NULL,
+    msgto           msg_addr_t      NOT NULL,
+    msg             msg_t           NOT NULL
 );
 
 CREATE TABLE targets (
-    id          serial      UNIQUE,
-    fqdn        fqdn_t      PRIMARY_KEY,
-    ip          ip_t,
-    cat         cat_t       NOT NULL,
-    zone        zone_t      NOT NULL,
-    free_ts     ts_t        NOT NULL DEFAULT 0,
-    curr_cli    cli_t       NOT NULL,
-    max_cli     cli_t       NOT NULL,
-    available   boolean     NOT NULL DEFAULT FALSE
+    id              serial          NOT NULL UNIQUE,
+    fqdn            fqdn_t          PRIMARY_KEY,
+    free_ts         ts_t            NOT NULL DEFAULT 0,
+    curr_cli        cli_t           NOT NULL,
+    max_cli         cli_t           NOT NULL,
+    available       boolean         NOT NULL DEFAULT FALSE
 );
 
+CREATE TABLE target_ips (
+    target_id       integer         NOT NULL REFERENCES targets (id),
+    ip              ip_t,           NOT NULL,
+    date_effective  timestamp       NOT NULL
+
+
 CREATE TABLE device_targets (
-    device_id   id_t        NOT NULL REFERENCES devices (id),
-    target_id   integer     NOT NULL REFERENCES targets (id),
-    priority    prio_t      NOT NULL DEFAULT 0,
+    device_id       id_t            NOT NULL REFERENCES devices (id),
+    target_id       integer         NOT NULL REFERENCES targets (id),
+    priority        prio_t          NOT NULL DEFAULT 0,
     PRIMARY KEY (device_id, target_id)
 );
 
 CREATE TABLE capabilities (
-    target_id   integer     NOT NULL REFERENCES targets (id),
-    service     mtype_t     NOT NULL REFERENCES mtypes (mtype),
-    info        info_t      NOT NULL,
+    target_id       integer         NOT NULL REFERENCES targets (id),
+    service         mtype_t         NOT NULL REFERENCES mtypes (mtype),
+    info            info_t          NOT NULL,
     PRIMARY KEY (target_id, service)
 );
 
 CREATE TABLE mtypes (
-    mtype       mtype_t     PRIMARY KEY,
-    mexclusive  boolean     NOT NULL
+    mtype           mtype_t         PRIMARY KEY,
+    mexclusive      boolean         NOT NULL
 );
 
 CREATE TABLE blacklist (
-    device_id   id_t        PRIMARY KEY
+    device_id       id_t            PRIMARY KEY
 );
 
