bab019749814ff26b5d198f24683a9a15b31f622
Stephen Woodrow <srwoodrow@gmail.com>
2012-01-29 18:31:12 -0500

    More database refactoring/renaming.

--- db/bismark_mgmt_tables.sql
+++ db/bismark_mgmt_tables.sql
@@ -1,39 +1,32 @@
 CREATE SCHEMA public;
 
-CREATE DOMAIN id_t AS varchar(50);
-CREATE DOMAIN version_t AS varchar(50);
-CREATE DOMAIN user_t AS varchar(50);
-CREATE DOMAIN ip_t AS inet;
-CREATE DOMAIN ts_t AS integer;
-CREATE DOMAIN cat_t AS varchar(50);
-CREATE DOMAIN msg_addr_t AS varchar(50);
-CREATE DOMAIN msg_t AS varchar(100);
-CREATE DOMAIN zone_t AS varchar(50);
-CREATE DOMAIN cli_t AS integer;
-CREATE DOMAIN prio_t AS integer;
-CREATE DOMAIN info_t AS varchar(500);
-CREATE DOMAIN mtype_t AS varchar(50);
-CREATE DOMAIN fqdn_t AS varchar(255);
+CREATE DOMAIN fqdn_t        AS varchar(255);    -- fully-qualified domain name
+CREATE DOMAIN id_t          AS varchar(50);     -- must allow 'BDM'
+CREATE DOMAIN ip_t          AS inet;
+CREATE DOMAIN msg_t         AS varchar(100);
+CREATE DOMAIN svcinfo_t     AS varchar(500);
+CREATE DOMAIN svcname_t     AS varchar(50);
+CREATE DOMAIN ts_t          AS integer;         -- UNIX timestamp
+CREATE DOMAIN version_t     AS varchar(50);
 
 CREATE TABLE devices (
     id              id_t            PRIMARY KEY,
     bversion        version_t       NOT NULL,
-    duser           user_t          NOT NULL,
     ip              ip_t            NOT NULL,
-    ts              ts_t            NOT NULL
+    last_seen_ts    ts_t            NOT NULL
 );
 
 CREATE TABLE tunnels (
     device_id       id_t            NOT NULL REFERENCES devices (id),
     port            integer         NOT NULL,
-    ts              ts_t            NOT NULL,
+    created_ts      ts_t            NOT NULL,
     PRIMARY KEY (device_id, port)
 );
 
 CREATE TABLE messages (
-    rowid           serial          PRIMARY KEY,
-    msgfrom         msg_addr_t      NOT NULL,
-    msgto           msg_addr_t      NOT NULL,
+    id              serial          PRIMARY KEY,
+    msgfrom         id_t            NOT NULL,
+    msgto           id_t            NOT NULL,
     msg             msg_t           NOT NULL
 );
 
@@ -41,37 +34,37 @@ CREATE TABLE targets (
     id              serial          NOT NULL UNIQUE,
     fqdn            fqdn_t          PRIMARY_KEY,
     free_ts         ts_t            NOT NULL DEFAULT 0,
-    curr_cli        cli_t           NOT NULL,
-    max_cli         cli_t           NOT NULL,
+    curr_cli        integer         NOT NULL,
+    max_cli         integer         NOT NULL,
     available       boolean         NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE device_targets (
+    device_id       id_t            NOT NULL REFERENCES devices (id),
+    target_id       integer         NOT NULL REFERENCES targets (id),
+    preference      integer         NOT NULL DEFAULT 0,
+    PRIMARY KEY (device_id, target_id)
 );
 
 CREATE TABLE target_ips (
     target_id       integer         NOT NULL REFERENCES targets (id),
     ip              ip_t,           NOT NULL,
     date_effective  timestamp       NOT NULL
-
-
-CREATE TABLE device_targets (
-    device_id       id_t            NOT NULL REFERENCES devices (id),
-    target_id       integer         NOT NULL REFERENCES targets (id),
-    priority        prio_t          NOT NULL DEFAULT 0,
-    PRIMARY KEY (device_id, target_id)
 );
 
-CREATE TABLE capabilities (
+CREATE TABLE target_services (
     target_id       integer         NOT NULL REFERENCES targets (id),
-    service         mtype_t         NOT NULL REFERENCES mtypes (mtype),
-    info            info_t          NOT NULL,
-    PRIMARY KEY (target_id, service)
+    service_id      integer         NOT NULL REFERENCES services (id),
+    info            svcinfo_t       NOT NULL,
+    PRIMARY KEY (target_id, service_id)
 );
 
-CREATE TABLE mtypes (
-    mtype           mtype_t         PRIMARY KEY,
-    mexclusive      boolean         NOT NULL
+CREATE TABLE services (
+    id              serial          NOT NULL UNIQUE,
+    name            svcname_t       PRIMARY KEY,
+    is_exclusive    boolean         NOT NULL
 );
 
 CREATE TABLE blacklist (
     device_id       id_t            PRIMARY KEY
 );
-
