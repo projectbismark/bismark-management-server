a543d829dba1f6561c0fc06087045542e5c000d2
Stephen Woodrow <srwoodrow@gmail.com>
2012-04-18 13:38:27 -0400

    Initial commit of direction fixup script.

---
+++ db/maintenance/direction_fixup.sql
@@ -0,0 +1,39 @@
+CREATE TEMP TABLE mserver_ips (
+    ip inet;
+)
+INSERT INTO mserver_ips VALUES ('4.71.254.153');
+INSERT INTO mserver_ips VALUES ('4.71.254.140');
+INSERT INTO mserver_ips VALUES ('216.156.197.146');
+INSERT INTO mserver_ips VALUES ('143.215.131.173');
+
+-- AGGL3BITRATE is currently screwed up
+--
+-- BEGIN;
+-- UPDATE m_aggl3bitrate
+-- SET direction = 'up'
+-- WHERE direction is NULL
+-- AND dstip IN (
+--     SELECT ip FROM mserver_ips
+-- );
+-- UPDATE m_aggl3bitrate
+-- SET direction = 'dw'
+-- WHERE direction is NULL
+-- AND srcip IN (
+--     SELECT ip FROM mserver_ips
+-- )
+-- COMMIT;
+
+BEGIN;
+UPDATE m_bitrate
+SET direction = 'up'
+WHERE direction is NULL
+AND dstip IN (
+    SELECT ip FROM mserver_ips
+);
+UPDATE m_bitrate
+SET direction = 'dw'
+WHERE direction is NULL
+AND srcip IN (
+    SELECT ip FROM mserver_ips
+)
+COMMIT;
