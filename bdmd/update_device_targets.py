0b5f77008c4140dac0269cb187025656bf542836
Stephen Woodrow <srwoodrow@gmail.com>
2012-05-16 12:43:47 -0400

    Add database update/insert. Refactoring.

--- bdmd/update_device_targets.py
+++ bdmd/update_device_targets.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import calendar
 import datetime
 import os
 import sys
@@ -14,8 +13,9 @@ UPDATE_FREQUENCY = datetime.timedelta(days=0)
 OLD_DEVICE_THRESHOLD = datetime.timedelta(days=30)
 FRESHNESS_THRESHOLD = datetime.timedelta(days=30)
 #FRESHNESS_THRESHOLD = datetime.timedelta(days=730)
-CLUSTER_PREFERENCES = [30, 20, 10]
+MLAB_GROUP_PREFERENCES = [30, 20, 10]
 MLAB_ONLY = True
+GLOBAL_UTCNOW = datetime.datetime.utcnow()
 
 REQ_ENV_VARS = ['VAR_DIR',
                 'BDM_PG_HOST',
@@ -105,7 +105,7 @@ class MserverDatabase(object):
 
     def lookup_ptr(self, ip, date_effective=None):
         if not date_effective:
-            date_effective = datetime.datetime.utcnow()
+            date_effective = GLOBAL_UTCNOW
         try:
             fqdn_list = self.fqdns_by_ip[ip]
             for f in fqdn_list:
@@ -117,7 +117,7 @@ class MserverDatabase(object):
 
     def lookup_a(self, fqdn, date_effective=None):
         if not date_effective:
-            date_effective = datetime.datetime.utcnow()
+            date_effective = GLOBAL_UTCNOW
         try:
             ip_list = self.ips_by_fqdn[fqdn]
             for i in ip_list:
@@ -151,86 +151,84 @@ def print_entry(f):
         return f(*args, **kwargs)
     return wrapper
 
-def find_update_candidates(dbconn):
-    cur = dbconn.cursor()
+def find_update_candidates(mgmt_dbconn):
+    cur = mgmt_dbconn.cursor()
     cur.execute((
             "SELECT t.id, t.min_date "
             "FROM ( "
             "   SELECT dt.device_id as id, min(dt.date_effective) as min_date "
             "   FROM device_targets as dt "
-            "   WHERE dt.device_id = dt.device_id "
-            "   AND dt.is_permanent = FALSE "
+            "   WHERE dt.is_permanent = FALSE "
             "   AND dt.is_enabled = TRUE "
             "   GROUP BY dt.device_id "
-            "   ) AS t "
-            "WHERE t.min_date < %s;"),
-            [(datetime.datetime.utcnow() - UPDATE_FREQUENCY).isoformat()])
+            "   ) AS t, devices AS d "
+            "WHERE t.min_date < %s "
+            "AND t.device_id = d.id "
+            "AND d.last_seen_ts >= %s;"),
+            [(GLOBAL_UTCNOW - UPDATE_FREQUENCY),
+             (GLOBAL_UTCNOW - OLD_DEVICE_THRESHOLD)])
     outofdate_devices = cur.fetchall()
-    cur.execute(
+    cur.execute((
             "SELECT t.did as id, NULL as min_date "
             "FROM ( "
             "   SELECT d.id as did, dt.device_id as dtid "
             "   FROM devices as d"
             "   LEFT OUTER JOIN device_targets as dt ON d.id = dt.device_id "
+            "   WHERE d.last_seen_ts >= %s "
             "   ) AS t "
-            "WHERE dtid IS NULL;")
+            "WHERE dtid IS NULL;"),
+            [(GLOBAL_UTCNOW - OLD_DEVICE_THRESHOLD)])
     unknown_devices = cur.fetchall()
-    cur.execute(
-            "SELECT devices.id FROM devices WHERE last_seen_ts < %s;",
-            [calendar.timegm((datetime.datetime.utcnow() -
-            OLD_DEVICE_THRESHOLD).timetuple())])
-    old_devices = [x[0] for x in cur.fetchall()]
-    return [x for x in (outofdate_devices + unknown_devices)
-            if x[0] not in old_devices]
+    devices = outofdate_devices + unknown_devices
+    return [d for d in devices if re.match('^OW[0-9A-F]{12}$', d[0], re.I)]
 
-def select_targets(mgmt_dbconn, data_dbconn, candidates, mserver_db):
+def select_device_targets(data_dbconn, candidates, mserver_db):
     dcur = data_dbconn.cursor()
-    device_targets = {}
-    candidates = [c for c in candidates if re.match('^OW[0-9A-F]{12}$', c[0])]
-    for c in candidates:
-        ordered_targets = []
+    device_targets = {}  # indexed by MAC address
+    for device_id, _ in candidates:
+        device_targets[device_id] = []
+        rtt_data = None
         dcur.execute((
                 "SELECT dstip, eventstamp, "
                 "   average, median, minimum, maximum, std "
                 "FROM m_mserver_rtt "
                 "WHERE deviceid = %s "
                 "AND eventstamp > %s "),
-                [c[0][2:], (datetime.datetime.utcnow() -
-                FRESHNESS_THRESHOLD).isoformat()])
+                [device_id[2:], (GLOBAL_UTCNOW - FRESHNESS_THRESHOLD)])
 
         if dcur.rowcount and dcur.rowcount > 0:
             # device has fresh mserver_rtt data
-            ordered_targets = ordered_targets_from_data(
-                    dcur.fetchall(), mserver_db)
+            rtt_data = dcur.fetchall()
         elif dcur.rowcount is not None and dcur.rowcount == 0:
-            # no rtt data, we need to ping from the servers
+            # no rtt data, we need to use mserver -> device ping data
             dcur.execute((
                     "SELECT dstip, eventstamp, "
                     "   average, median, minimum, maximum, std "
                     "FROM m_mserver_rping "
                     "WHERE deviceid = %s "
                     "AND eventstamp > %s "),
-                    [c[0][2:], (datetime.datetime.utcnow() -
-                    FRESHNESS_THRESHOLD).isoformat()])
+                    [device_id[2:], (GLOBAL_UTCNOW - FRESHNESS_THRESHOLD)])
             if dcur.rowcount and dcur.rowcount > 0:
-                ordered_targets = ordered_targets_from_data(
-                        dcur.fetchall(), mserver_db)
-            else:
-                print("ERROR: device '%s' has no RTT data." % c[0])
+                rtt_data = dcur.fetchall()
         else:
             print("ERROR: couldn't get db rowcount")
+            continue
 
-        device_targets[c[0]] = []
+        if not rtt_data:
+            print("ERROR: device '%s' has no RTT data." % device_id)
+            continue
+
+        ordered_targets = select_targets_by_rtt(rtt_data, mserver_db)
         if ordered_targets:
             if MLAB_ONLY:
-                device_targets[c[0]] = select_mlab_grouped_targets(
-                        ordered_targets, mdb)
+                device_targets[device_id] = select_mlab_targets_by_group(
+                        ordered_targets, mserver_db)
 
     return device_targets
 
-def ordered_targets_from_data(resultset, mserver_db):
+def select_targets_by_rtt(rtt_resultset, mserver_db):
     min_latency = {}
-    for row in resultset:
+    for row in rtt_resultset:
         fqdn = mserver_db.lookup_ptr(row[0], row[1])
         min_latency.setdefault(fqdn, []).append(row[4])
     median_minlatencies = []
@@ -240,41 +238,49 @@ def ordered_targets_from_data(resultset, mserver_db):
         median_minlatencies.append((fqdn, quick_median))
     median_minlatencies.sort(key=lambda x: x[1])
     pprint.pprint(median_minlatencies)
-    # TODO: return both fqdn and latency measured for logging/etc.
-    return [x[0] for x in median_minlatencies]
+    return median_minlatencies
 
-def ordered_targets_from_ping(deviceid, dbconn):
-    return None
-
-def select_mlab_grouped_targets(ordered_targets, mserver_db):
+def select_mlab_targets_by_group(ordered_targets, mserver_db):
     target_groups = []
     ranked_targets = []
-    for fqdn in ordered_targets:
+    for fqdn, _ in ordered_targets:
         m = re.search('(\w+)\.measurement-lab.org.$', fqdn)
-        if m and m.groups()[0] not in target_groups:
-            target_groups.append(m.groups()[0])
-            if len(target_groups) == len(CLUSTER_PREFERENCES):
+        try:
+            mlab_group = m.groups()[0]
+        except AttributeError:
+            mlab_group = None
+        if mlab_group and mlab_group not in target_groups:
+            target_groups.append(mlab_group)
+            if len(target_groups) == len(MLAB_GROUP_PREFERENCES):
                 break
     for i in xrange(len(target_groups)):
         for fqdn in mserver_db.fqdns_by_mlab_group[target_groups[i]]:
-            ranked_targets.append((fqdn, CLUSTER_PREFERENCES[i]))
+            ranked_targets.append((fqdn, MLAB_GROUP_PREFERENCES[i]))
     return ranked_targets
 
-if __name__ == '__main__':
-    config = {}
-    for evname in REQ_ENV_VARS:
-        try:
-            config[evname] = os.environ[evname]
-        except KeyError:
-            print_error(("Environment variable '%s' required and not defined. "
-                         "Terminating.") % evname)
-            sys.exit(1)
-    for (evname, default_val) in OPT_ENV_VARS:
-        config[evname] = os.environ.get(evname) or default_val
+def apply_device_targets(mgmt_dbconn, device_targets, mserver_db):
+    for device_id, ranked_targets in device_targets:
+        if ranked_targets:
+            cur = mgmt_dbconn.cursor()
+            cur.execute((
+                    "UPDATE device_targets "
+                    "SET is_enabled = FALSE "
+                    "WHERE is_enabled = TRUE "
+                    "AND is_permanent = FALSE "
+                    "AND device_id = %s;"),
+                    [device_id])
+            target_tuples = [
+                    (device_id, mserver_db.lookup_id(fqdn), pref,
+                    GLOBAL_UTCNOW) for (fqdn, pref) in ranked_targets]
+            cur.executemany((
+                    "INSERT INTO device_targets "
+                    "(device_id, target_id, preference, date_effective, "
+                    "is_enabled, is_permanent) "
+                    "VALUES (%s, %s, %s, %s, TRUE, FALSE);"),
+                    target_tuples)
+            mgmt_dbconn.commit()
 
-    print_debug = print_debug_factory(int(config['BDMD_DEBUG']) != 0)
-    print_debug(config)
-
+def main(config):
     mconn = psycopg2.connect(
             host=config['BDM_PG_HOST'],
             port=int(config['BDM_PG_PORT']),
@@ -292,25 +298,27 @@ if __name__ == '__main__':
 
     mdb = MserverDatabase(
             mconn,
-            start_date=(datetime.datetime.utcnow() - FRESHNESS_THRESHOLD))
+            start_date=(GLOBAL_UTCNOW - FRESHNESS_THRESHOLD))
     update_candidates = find_update_candidates(mconn)
     pprint.pprint(update_candidates)
-    updated_device_targets = select_targets(mconn, dconn, update_candidates, mdb)
-    pprint.pprint(updated_device_targets)
-    #update_targets(conn, updated_device_targets)
+    device_targets = select_device_targets(dconn, update_candidates, mdb)
+    pprint.pprint(device_targets)
+    apply_device_targets(mconn, device_targets, mdb)
 
 
-# set of tasks
-# 2. query for recent m_mserver_rtt for that device
-# 3. update based on target data, if available
-#       a. if new target, simply pick best m-lab cluster
-#       b. if existing target being retargeted, look for best average?
-# 4. if successful targets are available, then
-#       a. insert new rows into device_targets with current date_effective
-#       b. disable old non-permament rows
-# BEGIN;
-# UPDATE device_targets SET is_enabled = FALSE WHERE is_enabled = TRUE AND
-# is_permanent = FALSE;
-# INSERT INTO device_targets (device_id, target_id, preference, date_effective,
-# is_enabled) VALUES (%s, %s, %s, %s, TRUE)
-# COMMIT;
+if __name__ == '__main__':
+    config = {}
+    for evname in REQ_ENV_VARS:
+        try:
+            config[evname] = os.environ[evname]
+        except KeyError:
+            print_error(("Environment variable '%s' required and not defined. "
+                         "Terminating.") % evname)
+            sys.exit(1)
+    for (evname, default_val) in OPT_ENV_VARS:
+        config[evname] = os.environ.get(evname) or default_val
+
+    print_debug = print_debug_factory(int(config['BDMD_DEBUG']) != 0)
+    print_debug(config)
+
+    main(config)
